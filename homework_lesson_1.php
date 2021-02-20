2e2a989b7b373adc4db9302205d4d28aa796d0bf
sergey <sergey.seoz@gmail.com>
2021-02-20 22:01:32 +0200

    6/7

--- homework_lesson_1.php
+++ homework_lesson_1.php
@@ -189,3 +189,103 @@ echo 'Sort array desc: ' . implode(',', arrSortDesc($baseArr)) . '<br/>';
 
  * */
 echo '<br/>' . '==========#6==========' . '<br/>';
+
+$baseArr = [
+    ['1a', '2a'],
+    ['1b', '2b'],
+    ['1c', '2c']
+];
+
+function transpose($arr) {
+    for ($i = 0; $i < count($arr); $i++) {
+        for ($j = 0; $j < count($arr[$i]); $j++) {
+            $res[$j][$i] = $arr[$i][$j];
+        }
+    }
+    return $res;
+}
+echo '<pre>';
+var_dump(transpose($baseArr));
+echo '</pre>';
+
+function multiplyMatrix($multiplierArr1, $multiplierArr2) {
+    for ($i = 0; $i < count($multiplierArr1); $i++) {
+        for ($j = 0; $j < count($multiplierArr1[$i]); $j++) {
+            $res[$i][$j]=0;
+            for($k = 0; $k < count($multiplierArr2); $k++){
+                $res[$i][$j] += $multiplierArr1[$i][$k] * $multiplierArr2[$k][$j];
+            }
+        }
+    }
+    return $res;
+}
+$arr1 = [
+    [2, 3, 5],
+    [1, 6, 7]
+];
+$arr2 = [
+    [3, 2, 8],
+    [0, 7, 4]
+];
+echo '<pre>';
+var_dump(multiplyMatrix($arr1, $arr2));
+echo '</pre>';
+
+function deleteMatrixRow($arr) {
+    for ($i = 0; $i < count($arr); $i++) {
+        $rowSum = 0;
+        $isZero = false;
+        for ($j = 0; $j < count($arr[$i]); $j++) {
+            $rowSum += $arr[$i][$j];
+            if ($arr[$i][$j] == 0) {
+                $isZero = true;
+            }
+        }
+        if (!($rowSum > 0 && $isZero)) {
+            $res[] = $arr[$i];
+        }
+
+    }
+    return $res;
+}
+$deleteMatrixRowInputArr = [
+    [2, 3, 5],
+    [0, 7, 4],
+    [1, 6, 7]
+];
+echo '<pre>';
+var_dump(deleteMatrixRow($deleteMatrixRowInputArr));
+echo '</pre>';
+
+function deleteMatrixCol($arr) {
+    for ($i = 0; $i < count($arr); $i++) {
+        $colSum = 0;
+        $isZero = false;
+        for ($j = 0; $j < count($arr[$i]); $j++) {
+            $colSum += $arr[$j][$i];
+            if ($arr[$j][$i] == 0) {
+                $isZero = true;
+            }
+        }
+        echo $colSum . ' - ' . $isZero . ' / ';
+        if (!($colSum > 0 && $isZero)) {
+            for ($k = 0; $k < count($arr[$i]); $k++) {
+                $res[$k][] = $arr[$k][$i];
+            }
+        }
+    }
+    return $res;
+}
+$deleteMatrixColInputArr = [
+    [2, 3, 5],
+    [0, -7, 4],
+    [1, -6, 7]
+];
+echo '<pre>';
+var_dump(deleteMatrixCol($deleteMatrixColInputArr));
+echo '</pre>';
+
+/*Рекурсии
+Все задачи на циклы которые можно реализовать с помощью рекурсии, переписать с помощью рекурсивных функций
+Написать рекурсивную функцию которая будет обходить и выводить все значения любого массива и любого уровня вложенности*/
+echo '<br/>' . '==========#6==========' . '<br/>';
